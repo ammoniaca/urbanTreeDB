d5d67e7f498e5b00d493d08dd5d6f88f166a4ad3
ammoniaca <alessandro.montaghi@gmail.com>
2023-11-28 15:22:47 +0100

    Initial commit

---
+++ src/main/java/org/cnr/urbantreedb/UrbanTreeDbApplication.java
@@ -0,0 +1,13 @@
+package org.cnr.urbantreedb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanTreeDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbanTreeDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/cnr/urbantreedb/UrbanTreeDbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cnr.urbantreedb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanTreeDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
